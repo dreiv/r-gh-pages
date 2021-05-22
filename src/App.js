2507571fe2c3e04a6d06385cda3f2e4c6fa2c0f4
Andrei Voicu <dreivoicu@gmail.com>
2021-05-22 23:40:20 +0300

    build

--- src/App.js
+++ src/App.js
@@ -4,12 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
-          voluptates veniam commodi sint et laborum, facilis rerum dolor tempora
-          laboriosam. Impedit sunt pariatur in officia. Error tempore deserunt
-          iste perspiciatis!
-        </p>
+        <h1>hello world!</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
