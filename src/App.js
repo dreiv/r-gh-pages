d6ac940a66b80f004632ff62f8dfb78d5099aba0
Andrei Voicu <dreivoicu@gmail.com>
2021-05-22 23:20:47 +0300

    test build 5

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>hello world! :S</h1>
+        <h1>hello worlD!</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
